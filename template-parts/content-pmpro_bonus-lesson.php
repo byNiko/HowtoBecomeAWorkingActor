8505aeeff65d3b6c108eabe454b91ccff7cd5af8
nick <nick@byniko.com>
2024-12-03 07:26:53 -0800

    update title bonus lesson

--- template-parts/content-pmpro_bonus-lesson.php
+++ template-parts/content-pmpro_bonus-lesson.php
@@ -16,6 +16,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+	<?php the_title( '<h4 class="entry-title">', '</h4>' );	?>
 		<?= byniko_membership\bonus_lesson_content($post); ?>
 		<?php // the_content(); ?>
 	</div><!-- .entry-content -->
@@ -23,9 +24,7 @@
 	
 
 	<footer class="entry-footer">
-	<?php
-			the_title( '<h4 class="entry-title">', '</h4>' );
-			?>
+	
 		<?php //byniko_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
