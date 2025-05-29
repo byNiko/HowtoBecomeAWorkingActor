39eee706dc0a43d791f3fc078aab64ff17ea7c0c
nick <nick@byniko.com>
2025-05-29 07:22:54 -0700

    capture free level

--- template-parts/front-page/capture.php
+++ template-parts/front-page/capture.php
@@ -2,29 +2,39 @@
 	<div class="row">
 		<header class="">
 			<h4>
-				<div class="display-md centered"> Free Lesson</div>
+				<div class="display-md centered">A Free Gift For You</div>
 			</h4>
 		</header>
 
 		<div class="content">
-			<div class="grid-50">
+			<div class="grid">
+
 				<div>
-					<header class="fz-md fw-semibold">
-					Thanks for your interest!
+					<header class="fz-md fw-semibold centered">
+						Thanks for your interest.
 					</header>
-
-					<p>
-					As a way of thanking you for you time and interest - we'd like to extend to you your first teaching free of charge. </p>
-					<p>By signing up, you'll also be the first to know about course updates, opportunities to meet with Jim, new free videos and more.</p>
-
-				</div>
-				<div class="centered">
-				<?php echo FrmFormsController::get_form_shortcode( array( 'id' => 7 ) ); ?>
-					<!-- <form action="" class="grid">
+					<p>I'd love to work with you, but no matter what, your success is important to me.
+					<p>Sign up for a free membership and I'll give you access to <strong>ten essential lessons</strong> that I know will help you – completely free. </p>
+					<div class="centered">
+						<?php echo do_shortcode('[pmpro_signup level="1" short="emailonly" login="false" title="true"]'); ?>
+						<?php // echo FrmFormsController::get_form_shortcode(array('id' => 7)); ?>
+						<!-- <form action="" class="grid">
 						<input type="email" placeholder="Enter your email">
 						<button class="button hollow mt-3">Sign Up</button>
 					</form> -->
+					</div>
+					<p>These are just a few of the topics you'll get:</p>
+					<ul>
+						<li>Dealing with criticism and being a perfectionist</li>
+						<li>How to deal with stage fright</li>
+						<li>How to deal with your fears and doubts about going after this</li>
+						<li>How to find a good teacher</li>
+					</ul>
+
+					<p>By signing up, you'll also be the first to know about course updates, opportunities to meet with Jim, new free videos and more.</p>
+
 				</div>
+
 			</div>
 		</div>
-		</div>
+	</div>
