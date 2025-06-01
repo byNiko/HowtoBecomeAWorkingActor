4ded8113737baa55378d4f25906faf2a307988c7
nick <nick@byniko.com>
2025-06-01 10:45:42 -0700

    username shortcode

--- functions.php
+++ functions.php
@@ -496,6 +496,6 @@ function display_current_user_display_name() {
 	// var_dump($user);
 	$email = $user->user_email;
 	$display_name = $user->display_name;
-	return $display_name ?? $email;
+	echo $display_name ?? $email;
 }
 add_shortcode('show_current_user_name', 'display_current_user_display_name');
